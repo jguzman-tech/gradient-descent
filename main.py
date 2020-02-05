d2b0470b5e47a71eb1a15f645ca491d920e3d128
Joseph Guzman ~ student <jfg95@wind.nauhpc>
2020-02-04 19:44:25 -0700

    Made parse function skeleton

--- main.py
+++ main.py
@@ -28,38 +28,49 @@ def GradientDescent(X, y, stepSize, maxiterations):
         weightMatrix[:, k - 1] = newWeightVector
     return weightMatrix
 
+def parse(fname, seed):
+    if(fname == 'spam.data'):
+        all_rows = []
+        with open('spam.data') as fp:
+            for line in fp:
+                row = line.split(' ')
+                all_rows.append(row)
+        temp_ar = np.array(all_rows, dtype=float)
+        # standardize each column to have μ = 0 and σ^(2) = 1
+        # in other words convert all elements to z-scores for each column
+        for col in range(temp_ar.shape[1] - 1): # for all but last column (output)
+            temp_ar[:, col] = stats.zscore(temp_ar[:, col])
+        np.random.seed(seed)
+        np.random.shuffle(temp_ar) # shuffle rows, set of columns remain the same
+    elif(fname == 'SAheart.data'):
+        # Make sure to skip the first row
+        # Make sure to replace the present and absent strings
+        # Get the zscore if you want to be complete
+        # Drop the row column
+        pass
+    elif(fname == 'zip.train'):
+        pass
+    else:
+        raise Exception("Unknown dataset")
+    return temp_ar
+
 X = None
 y = None
-epsilon = float(sys.argv[2])
+stepSize = float(sys.argv[2])
 maxiterations = int(sys.argv[3])
 seed = int(sys.argv[4])
 print("sys.argv = " + str(sys.argv))
 # TODO: Parsing should be wrapped into a function
-#       we want: X, y = parser(fname)
-if len(sys.argv) < 2 or sys.argv[1] not in {'spam.data', 'SAheart.data', 'zip.train'}:
-    print("Execution example: main.py <dataset>")
+#       we want: temp_ar = parse(fname)
+if len(sys.argv) < 5 or sys.argv[1] not in {'spam.data', 'SAheart.data', 'zip.train'}:
+    print("Execution example: python3 main.py <dataset> <stepSize> <maxiterations> <seed>")
     print("The valid dataset values are: spam.data, SAheart.data, and zip.train.")
+    print("stepSize must be a float")
+    print("maxiterations must be an int")
+    print("seed must be an int")
     exit(0)
-elif sys.argv[1] == 'spam.data':
-    all_rows = []
-    with open('spam.data') as fp:
-        for line in fp:
-            row = line.split(' ')
-            all_rows.append(row)
-    temp_ar = np.array(all_rows, dtype=float)
-    # standardize each column to have μ = 0 and σ^(2) = 1
-    # in other words convert all elements to z-scores for each column
-    for col in range(temp_ar.shape[1] - 1): # for all but last column (output)
-        temp_ar[:, col] = stats.zscore(temp_ar[:, col])
-    np.random.seed(seed)
-    np.random.shuffle(temp_ar) # shuffle rows, set of columns remain the same
-elif sys.argv[1] == 'SAheart.data':
-    # Make sure to replace the present and absent strings
-    # Get the zscore if you want to be complete
-    # Drop the row column
-    pass
-elif sys.argv[1] == 'zip.train':
-    pass
+
+temp_ar = parse(sys.argv[1], seed)
 
 # temp_ar is randomly shuffled at this point
 num_rows = temp_ar.shape[0]
@@ -77,17 +88,17 @@ validation_X = X[int(num_rows * 0.8):]                     # slice of 80% to 100
 validation_y = y[int(num_rows * 0.8):]                     # slice of 80% to 100%
 
 print('            y')
-print('set       0     1')
+print('  {0: >10} {1: >4} {2: >4}'.format('set', '0', '1'))
 print('  {0: >10} {1: >4} {2: >4}'.format('test',
-                                          str((test_y == 1).sum()),
-                                          str((test_y == 0).sum())))
+                                          str((test_y == 0).sum()),
+                                          str((test_y == 1).sum())))
 print('  {0: >10} {1: >4} {2: >4}'.format('train',
-                                          str((train_y == 1).sum()),
-                                          str((train_y == 0).sum())))
+                                          str((train_y == 0).sum()),
+                                          str((train_y == 1).sum())))
 print('  {0: >10} {1: >4} {2: >4}'.format('validation',
-                                          str((validation_y == 1).sum()),
-                                          str((validation_y == 0).sum())))
-weightMatrix = GradientDescent(train_X, train_y, epsilon, maxiterations)
+                                          str((validation_y == 0).sum()),
+                                          str((validation_y == 1).sum())))
+weightMatrix = GradientDescent(train_X, train_y, stepSize, maxiterations)
 
 validation_predict = np.matmul(validation_X, weightMatrix)
 validation_predict[validation_predict < 0.0] = 0
@@ -108,7 +119,7 @@ plt.plot(validation_error, c="red", label="validation")
 plt.plot(train_error, c="blue", label="train")
 plt.xlabel('% Error')
 plt.ylabel('Iteration')
-plt.savefig(sys.argv[1] + "_itr_" + str(maxiterations)+ "_step_" + str(epsilon) + "_seed_" + str(seed) + "plot.png")
+plt.savefig(sys.argv[1] + "_itr_" + str(maxiterations)+ "_step_" + str(stepSize) + "_seed_" + str(seed) + "plot.png")
     
 print("Done!")
 # import pdb; pdb.Pdb().set_trace() # break into pdb
